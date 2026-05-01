e66f197b4269fdf884cd0f921500992d33deaa6a
Alteza13 <antirusher13@gmail.com>
2026-05-01 20:28:36 +0500

    started creating game core

---
+++ core/block.py
@@ -0,0 +1,9 @@
+from ursina import Button, color
+
+class Block(Button):
+    def __init__(self, position=(0,0,0)):
+        super().__init__(
+            model='cube',
+            color=color.green,
+            position=position
+        )
---
+++ core/player.py
@@ -0,0 +1,5 @@
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+class Player(FirstPersonController):
+    def __init__(self):
+        super().__init__()
