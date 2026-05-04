5f0467212ae5e617dba4869dbfd6974f5ab01af6
Alteza13 <antirusher13@gmail.com>
2026-05-05 03:48:23 +0500

    added respawn if u have fallen from the start

--- core/player.py
+++ core/player.py
@@ -1,5 +1,17 @@
 from ursina.prefabs.first_person_controller import FirstPersonController
+from ursina import Vec3
 
 class Player(FirstPersonController):
     def __init__(self, position=(0, 2, 0)):
         super().__init__(position=position)
+
+        self.spawn_point = Vec3(position)
+
+    def update(self):
+        super().update()
+
+        if self.y < -10:
+            self.respawn()
+
+    def respawn(self):
+        self.position = self.spawn_point
