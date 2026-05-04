d6c67c7849f150d990ded3060958b963c67e9013
Alteza13 <antirusher13@gmail.com>
2026-05-04 22:35:47 +0500

    menu upgrade and test

--- ui/menu.py
+++ ui/menu.py
@@ -2,13 +2,37 @@ from ursina import *
 
 class Menu(Entity):
     def __init__(self, game):
-        super().__init__(parent=camera.ui)
+        super().__init__()
 
         self.game = game
+        self.parent = camera.ui
 
-        self.play_button = Button(text="Play", y=0.2, on_click=self.play)
-        self.settings_button = Button(text="Settings", y=0)
-        self.exit_button = Button(text="Exit", y=-0.2, on_click=application.quit)
+      
+        self.bg = Entity(
+            parent=self,
+            model='quad',
+            scale=(2, 1),
+            color=color.black66
+        )
+
+       
+        self.play_button = Button(
+            text="Play",
+            parent=self,
+            scale=(0.3, 0.1),
+            y=0.2
+        )
+        self.play_button.on_click = self.play
+
+        self.exit_button = Button(
+            text="Exit",
+            parent=self,
+            scale=(0.3, 0.1),
+            y=-0.2
+        )
+        self.exit_button.on_click = application.quit
 
     def play(self):
+        print("PLAY CLICKED")
+        self.disable()
         self.game.set_state("playing")
