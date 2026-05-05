2b9a1e08b03ad89dfbcaf1c5a3009ba6d784e400
Alteza13 <antirusher13@gmail.com>
2026-05-06 02:25:12 +0500

    new hotbar

--- ui/hotbar.py
+++ ui/hotbar.py
@@ -3,32 +3,76 @@ from ursina import *
 class Hotbar(Entity):
     def __init__(self, game):
         super().__init__(parent=camera.ui)
-        self.game = game
 
+        self.game = game
         self.selected_index = 0
         self.slots = []
 
-        self.create_slots()
+        self.items = [
+            'textures/grass',
+            'textures/stone',
+            'textures/dirt',
+            'textures/grass',
+            'textures/stone',
+            'textures/dirt',
+            'textures/grass',
+            'textures/stone',
+            'textures/dirt',
+            'textures/grass',
+        ]
 
-    def create_slots(self):
-        for i in range(5):
-            slot = Entity(
+        self.build()
+
+    def build(self):
+        start_x = -0.45
+        spacing = 0.09
+
+        for i in range(10):
+
+            slot = Button(
                 parent=self,
                 model='quad',
-                color=color.dark_gray,
+                texture=self.items[i],
+                color=color.gray,
                 scale=(0.08, 0.08),
-                x=-0.2 + i * 0.1,
-                y=-0.45
+                position=(start_x + i * spacing, -0.45),
+                highlight_color=color.light_gray,
+                pressed_color=color.white
             )
+
+            slot.frame = Entity(
+                parent=slot,
+                model='quad',
+                color=color.gray,
+                scale=1.1,
+                z=0.01
+            )
+
             self.slots.append(slot)
 
-        self.update_ui()
+        self.update_visual()
 
     def input(self, key):
-        if key in ['1','2','3','4','5']:
-            self.selected_index = int(key) - 1
-            self.update_ui()
+        if key.isdigit():
+            i = int(key)
+            if i == 0:
+                i = 9
+            else:
+                i -= 1
 
-    def update_ui(self):
+            self.selected_index = i
+            self.update_visual()
+
+    def update_visual(self):
         for i, slot in enumerate(self.slots):
-            slot.color = color.white if i == self.selected_index else color.dark_gray
+
+            if i == self.selected_index:
+                slot.frame.color = color.white
+            else:
+                slot.frame.color = color.gray
+    # def update_selection(self):
+    #     for i, slot in enumerate(self.slots):
+    #         if i == self.selected_index:
+    #             slot.color = color.white
+    #         else:
+    #             slot.color = color.gray
