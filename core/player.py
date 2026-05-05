a9ee8b9fc6ebf53b4be6be646baa1e74d6ccd97e
Alteza13 <antirusher13@gmail.com>
2026-05-06 01:56:03 +0500

    fixed bug with no jump

--- core/player.py
+++ core/player.py
@@ -8,65 +8,55 @@ class Player(FirstPersonController):
         self.game = game
         self.spawn_point = Vec3(position)
 
-        self.crosshair = Entity(parent=camera.ui)
-        Entity(
-            parent=camera.ui,
-            model='quad',
-            scale=(0.03, 0.003),
-            color=color.white,
-            texture=None,
-            collider=None
-        )
+        # self.crosshair_h = Entity(
+        #     parent=camera.ui,
+        #     model='quad',
+        #     color=color.white,
+        #     scale=(0.02, 0.002)
+        # )
 
-        self.crosshair = Entity(
-            parent=camera.ui,
-            model='quad',
-            scale=(0.003, 0.03),
-            color=color.white,
-            texture=None,
-            collider=None
-        )
+        # self.crosshair_v = Entity(
+        #     parent=camera.ui,
+        #     model='quad',
+        #     color=color.white,
+        #     scale=(0.002, 0.02)
+        # )
+        # Text(
+        #     text='+',
+        #     parent=camera.ui,
+        #     origin=(0, 0),
+        #     scale=2,
+        #     color=color.white
+        # )
+        # self.hit_info = None
 
     def update(self):
         super().update()
 
-        hit_info = raycast(
+        self.hit_info = raycast(
             origin=camera.world_position,
             direction=camera.forward,
-            distance=5
+            distance=5,
+            ignore=[self]
         )
-
-        # if hit_info.hit:
-        #     if held_keys['left mouse']:
-        #         destroy(hit_info.entity)
-
-        #     if held_keys['right mouse']:
-        #         pos = hit_info.entity.position + hit_info.normal
-        #         block_class = self.game.get_selected_block()
-        #         block_class(position=pos)
-        
 
         if self.y < -100:
             self.respawn()
 
     def input(self, key):
-        hit_info = raycast(
-            origin=camera.world_position,
-            direction=camera.forward,
-            distance=5
-        )
-
-        if not hit_info.hit:
-            return
 
         if key == 'left mouse down':
-            destroy(hit_info.entity)
+            if self.hit_info and self.hit_info.hit:
+                destroy(self.hit_info.entity)
 
-        if key == 'right mouse down':
-            pos = hit_info.entity.position + hit_info.normal
-            block_class = self.game.get_selected_block()
-            block_class(position=pos)
+        elif key == 'right mouse down':
+            if self.hit_info and self.hit_info.hit:
+                pos = self.hit_info.entity.position + self.hit_info.normal
+                block_class = self.game.get_selected_block()
+                block_class(position=pos)
+
+        super().input(key)
 
     def respawn(self):
         self.position = self.spawn_point
-        self.velocity = Vec3(0,0,0)
+        self.velocity = Vec3(0, 0, 0)
